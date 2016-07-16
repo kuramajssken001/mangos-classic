6d9d9516f555f3c64e2996d3c7c65a02e5a4a43b
kuramajssken001 <kuramajssken1234567@gmail.com>
2016-07-16 20:53:00 +0800

    修复德鲁伊技能豹之迅捷

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4085,7 +4085,7 @@ void Aura::HandleShapeshiftBoosts(bool apply)
         Unit::SpellAuraHolderMap& tAuras = target->GetSpellAuraHolderMap();
         for (Unit::SpellAuraHolderMap::iterator itr = tAuras.begin(); itr != tAuras.end();)
         {
-            if (itr->second->IsRemovedOnShapeLost())
+			if (itr->second->IsRemovedOnShapeLost() || itr->second->GetSpellProto()->Id == 24864)
             {
                 target->RemoveAurasDueToSpell(itr->second->GetId());
                 itr = tAuras.begin();
